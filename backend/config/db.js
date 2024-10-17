7bd77ce7f4f63c53245bb204bf7267928b285edc
Saad Ezz <73346617+saad6680@users.noreply.github.com>
2024-10-17 11:54:31 +0300

    Add files via upload

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1); // Exit the process with an error code
+  }
+}
+
+module.exports = connectDB;
